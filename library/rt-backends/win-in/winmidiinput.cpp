c346c2474b355a64ebb396c8a1f122dd4372ee90
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2014-11-22 12:03:17 +0000

    fix windows input: noteon with velocity 0 is noteoff
    git-svn-id: https://svn.code.sf.net/p/drumstick/code/trunk@300 b2133143-ca9b-4e53-b430-e31f198fbdac

--- library/rt-backends/win-in/winmidiinput.cpp
+++ library/rt-backends/win-in/winmidiinput.cpp
@@ -149,6 +149,8 @@ namespace rt {
 
         void emitSignals(int status, int channel, int data1, int data2)
         {
+            if (status == MIDI_STATUS_NOTEON && data2 == 0)
+                status = MIDI_STATUS_NOTEOFF;
             switch (status) {
             case MIDI_STATUS_NOTEOFF:
                 if(m_out != 0 && m_thruEnabled)
