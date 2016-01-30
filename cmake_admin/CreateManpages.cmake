ae4ed0c085bc2c34cd13787afd51dc35a9793736
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2016-01-30 20:59:51 +0000

    From: Heiko Becker <heirecka@exherbo.org> Date: Sat, 30 Jan 2016 17:10:11 +0100 Subject: [PATCH] Use GNUInstallDirs to install arch-independent data
    git-svn-id: https://svn.code.sf.net/p/drumstick/code/trunk@316 b2133143-ca9b-4e53-b430-e31f198fbdac

--- cmake_admin/CreateManpages.cmake
+++ cmake_admin/CreateManpages.cmake
@@ -31,5 +31,5 @@ MACRO(CREATE_MANPAGES)
     ENDFOREACH (it)
     ADD_CUSTOM_TARGET(manpages ALL DEPENDS ${outfiles})
     INSTALL ( FILES ${outfiles}
-              DESTINATION share/man/man1 )
+              DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/man/man1" )
 ENDMACRO(CREATE_MANPAGES)
