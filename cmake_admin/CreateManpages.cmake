bd1284546ab5b97e40f64b1e7f485e3a1b8521fe
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2017-08-14 20:59:48 +0000

    * fixed unit test for more tolerance * release preparations
    git-svn-id: https://svn.code.sf.net/p/drumstick/code/trunk@344 b2133143-ca9b-4e53-b430-e31f198fbdac

--- cmake_admin/CreateManpages.cmake
+++ cmake_admin/CreateManpages.cmake
@@ -1,9 +1,9 @@
 # MIDI Sequencer C++ Library 
-# Copyright (C) 2008-2016 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
+# Copyright (C) 2008-2017 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
