722927b6a01f2288bb58f1ad2065069f804e1177
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2019-12-11 06:55:44 +0000

    Modernization of the CMake build system
    git-svn-id: https://svn.code.sf.net/p/drumstick/code/trunk@395 b2133143-ca9b-4e53-b430-e31f198fbdac

--- library/drumstick-config.cmake
+++ library/drumstick-config.cmake
@@ -1,4 +1,8 @@
-foreach(component ${drumstick_FIND_COMPONENTS})
-    string(TOLOWER ${component} comp)
-    include(${CMAKE_CURRENT_LIST_DIR}/drumstick-${comp}-config.cmake)
+if (CMAKE_VERSION VERSION_LESS 3.1.0)
+    message(FATAL_ERROR "Drumstick requires at least CMake version 3.1.0")
+endif()
+
+foreach(component ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS})
+    string(TOLOWER ${component} locomp)
+    include(${CMAKE_CURRENT_LIST_DIR}/drumstick-${locomp}-config.cmake)
 endforeach()
