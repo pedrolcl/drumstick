914aad948e35a7e27f34aada36e7fc20074d1346
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2023-08-17 15:58:46 +0200

    CMake config check to avoid mixing Qt6 with Qt5

--- library/drumstick-config.cmake
+++
@@ -1,8 +0,0 @@
-if (CMAKE_VERSION VERSION_LESS 3.1.0)
-    message(FATAL_ERROR "Drumstick requires at least CMake version 3.1.0")
-endif()
-
-foreach(component ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS})
-    string(TOLOWER ${component} locomp)
-    include(${CMAKE_CURRENT_LIST_DIR}/drumstick-${locomp}-config.cmake)
-endforeach()
